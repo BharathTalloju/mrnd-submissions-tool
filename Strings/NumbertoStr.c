43e0a2a4375515d1733dee0a1d21ffb3a945f0c8
BharathTalloju <tallojubharath@gmail.com>
2016-02-27 21:58:04 +0530

    Finished

--- Strings/NumbertoStr.c
+++ Strings/NumbertoStr.c
@@ -1,5 +1,5 @@
 /*
-OVERVIEW: Given a float number ,Convert it into a string 
+OVERVIEW: Given a float number ,Convert it into a string
 
 INPUTS: A Float,Final string ,How many digits after decimal has to be displayed .
 Its zero for integers.
@@ -19,7 +19,54 @@ NOTES: Don't create new string.
 
 #include <stdio.h>
 
+int return_integer_part_in_reverse(float number);
 
-void number_to_str(float number, char *str,int afterdecimal){
-	
+void number_to_str(float number, char *result,int afterdecimal){
+	int writer = 0;
+	int integer_part = return_integer_part_in_reverse(number);
+
+	number = number - (int)number;
+	printf("Number = %f\n", number);
+	while (integer_part) {
+		result[writer++] = '0' + (integer_part % 10);
+		integer_part = integer_part/10;
+	}
+
+	if(afterdecimal){
+		result[writer++] = '.';
+
+		while (afterdecimal) {
+			printf("\nnumber writing = %f\n",number*10 );
+			result[writer++] = '0' + (int)(number * 10);
+			number = number*10 - (int)(number*10);
+			afterdecimal--;
+		}
+	}
+	result[writer] = '\0';
+
+
+}
+
+int return_integer_part_in_reverse(float number){
+	int n = number;
+	int result = 0;
+
+	while (n) {
+		result = result*10 + (n%10);
+		n /= 10;
+	}
+
+	return result;
+
+}
+
+int main(int argc, char const *argv[]) {
+
+	float number = 524.7865;
+	int afterdecimal = 2;
+	char str[50];
+
+	number_to_str(number,str, afterdecimal);
+	printf("%f = %s",number,str );
+	return 0;
 }
