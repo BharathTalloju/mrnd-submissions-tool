9bc1fd38510aa666bc69e4309da7a1e70cf25dd3
BharathTalloju <tallojubharath@gmail.com>
2016-04-05 16:59:56 +0530

    Finished

--- BST/src/BSTTransversals.c
+++ BST/src/BSTTransversals.c
@@ -15,7 +15,7 @@ it and understand how testing works .
 */
 
 #include <stdio.h>
-
+#include <stdlib.h>
 
 struct node{
 	struct node * left;
@@ -33,6 +33,7 @@ void inorderBST(struct node *root, int *arr, int first_call){
 	}
 	inorderBST(root-> left, arr, 0);
 	arr[i++] = root-> data;
+	printf(" %d at arr[%d], ", arr[i-1], i-1);
 	inorderBST(root-> right, arr, 0);
 }
 
@@ -48,7 +49,9 @@ void preorderBST(struct node *root, int *arr, int flag_first_call){
 	if(root == NULL){
 		return;
 	}
+
 	arr[i++] = root-> data;
+	printf("%d at arr[%d], ", arr[i-1], i-1);
 	preorderBST(root-> left, arr, 0);
 	preorderBST(root-> right, arr, 0);
 }
@@ -71,6 +74,7 @@ void postorderBST(struct node *root, int *arr, int first_call){
 	postorderBST(root-> left, arr, 0);
 	postorderBST(root-> right, arr, 0);
 	arr[i++] = root-> data;
+	printf("%d at arr[%d], ", arr[i-1], i-1);
 }
 
 void postorder(struct node *root, int *arr){
@@ -78,11 +82,64 @@ void postorder(struct node *root, int *arr){
 }
 
 //Tests
+
+struct node * new_node_spec(int data){
+	struct node *temp = (struct node *)malloc(sizeof(struct node));
+	temp->data = data;
+	temp->left = NULL;
+	temp->right = NULL;
+	return temp;
+}
+struct node * add_node_spec(struct node *root, int data){
+	if (root == NULL) return new_node_spec(data);
+
+	if (data < root->data)
+		root->left = add_node_spec(root->left, data);
+	else if (data > root->data)
+		root->right = add_node_spec(root->right, data);
+
+	return root;
+}
+
+
 int main(int argc, char const *argv[]) {
 
 	//Test1
 	{
-		
+		struct node *root = NULL;
+		int nums[10] = { 10, 5, 12, 12, 15 };
+		int arr[20] = {0};
+		int len = 5;
+
+		for (int i = 0; i < 5; i++){
+			root = add_node_spec(root, nums[i]);
+		}
+		printf("\nInorder: \n" );
+		inorder(root, arr);
+
+		//for(int i = 0; i< 20 ; i++){
+			//printf("%d", arr[i]);
+		//}
+
+		printf("\nPreordrer\n" );
+		preorder(root, arr);
+
+		//for(int i = 0; i< 20 ; i++){
+		//	printf("%d", arr[i]);
+		//}
+
+		printf("\nPostorder: \n" );
+		postorder(root, arr);
+		//for(int i = 0; i< 20 ; i++){
+		//	printf("%d", arr[i]);
+		//}
+	}
+	//Test2
+	{
+		printf("\n\t\t\tTest2\n");
+		int arr[2] = {0};
+		inorder(NULL, arr);
+		preorder(NULL, arr);
 	}
 	return 0;
 }
